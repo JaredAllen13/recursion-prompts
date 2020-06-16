dd0dafb944e03691a1c516009d9faef1941c1de3
Jared Allen <jaredallen13@gmail.com>
2020-06-16 14:45:39 -0600

    complete the isEven function to return if a number is even using recursion

--- src/recursion.js
+++ src/recursion.js
@@ -40,8 +40,13 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
-};
-
+    n = Math.abs(n);
+    if (n === 0) {return true};
+    if (n === 1) {return false};
+    if (n > 0){
+      return isEven(n - 2);
+    }
+  };
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
